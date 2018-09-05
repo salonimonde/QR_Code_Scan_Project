762bc070fac1384bbc95cf5c9936db586a7ee846
Avinesh Gupta <avinesh@bynry.com>
2018-09-05 11:27:38 +0530

    QR Code Scan Project

---
+++ app/src/main/java/com/example/bynry_01/qr_code_scan_project/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.bynry_01.qr_code_scan_project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnClickHereToScan;
+    private TextView txtValues;
+
+    private IntentIntegrator qrScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        txtValues = findViewById(R.id.txt_values);
+        btnClickHereToScan = findViewById(R.id.btn_click_here_to_scan);
+        btnClickHereToScan.setOnClickListener(this);
+
+        qrScan = new IntentIntegrator(this);
+
+    }
+
+    protected void OnActivityResult(int requestCode, int resultCode, Intent data){
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, requestCode, data);
+        if(result != null){
+
+            if(result.getContents() == null){
+
+                Toast.makeText(this,"Result Not Found",Toast.LENGTH_LONG).show();
+            }
+            else{
+                try{
+                    String obj = result.getContents();
+
+                    txtValues.setText(obj);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+
+                    Toast.makeText(this,result.getContents(),Toast.LENGTH_LONG).show();
+                }
+            }
+
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        qrScan.initiateScan();
+    }
+}
---
+++ app/src/test/java/com/example/bynry_01/qr_code_scan_project/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.bynry_01.qr_code_scan_project;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
